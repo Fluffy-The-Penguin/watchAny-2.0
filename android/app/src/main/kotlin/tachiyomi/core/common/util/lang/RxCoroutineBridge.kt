855ee2eee5d28056f1b4fbf1497b72d7bad09cd5
Fluffy-The-Penguin <aryanpanwar2603@gmail.com>
2026-07-25 22:20:29 +0530

    v2.1.0 - Major Release: Full Mihon ClassLoader & RxJava Coroutine Bridge Fix

--- android/app/src/main/kotlin/tachiyomi/core/common/util/lang/RxCoroutineBridge.kt
+++ android/app/src/main/kotlin/tachiyomi/core/common/util/lang/RxCoroutineBridge.kt
@@ -6,8 +6,28 @@ import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
 suspend fun <T> Observable<T>.awaitSingle(): T = suspendCoroutine { continuation ->
+    var isResumed = false
+    var hasValue = false
     subscribe(
-        { value -> continuation.resume(value) },
-        { error -> continuation.resumeWithException(error) },
+        { value ->
+            if (!isResumed) {
+                isResumed = true
+                hasValue = true
+                continuation.resume(value)
+            }
+        },
+        { error ->
+            if (!isResumed) {
+                isResumed = true
+                continuation.resumeWithException(error)
+            }
+        },
+        {
+            if (!isResumed && !hasValue) {
+                isResumed = true
+                continuation.resumeWithException(NoSuchElementException("Observable completed empty"))
+            }
+        }
     )
 }
+
