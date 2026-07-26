d209041abb77ce60c0102a4251320402481e7e8b
Fluffy-The-Penguin <aryanpanwar2603@gmail.com>
2026-07-27 01:57:00 +0530

    v2.1.22 - Lock kotlinx-serialization to 1.6.3 for Tachiyomi/Mihon extension binary compatibility

--- installer.iss
+++ installer.iss
@@ -1,6 +1,6 @@
 [Setup]
 AppName=watchAny
-AppVersion=2.1.21
+AppVersion=2.1.22
 DefaultDirName={autopf}\watchAny
 DefaultGroupName=watchAny
 OutputDir=build\windows
--- lib/services/update_service.dart
+++ lib/services/update_service.dart
@@ -69,7 +69,7 @@ class UpdateService extends ChangeNotifier {
   factory UpdateService() => _instance;
   UpdateService._internal();
 
-  static const String currentVersion = '2.1.21';
+  static const String currentVersion = '2.1.22';
 
 
 
