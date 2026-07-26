96ebaf7f1fc0a8c98902201261d8b4c6003ab4f5
Fluffy-The-Penguin <aryanpanwar2603@gmail.com>
2026-07-27 01:07:58 +0530

    v2.1.20 - Complete Mihon extension network support with okhttp3.brotli.Brotli integration

---
+++ android/app/src/main/kotlin/okhttp3/brotli/Brotli.kt
@@ -0,0 +1,21 @@
+package okhttp3.brotli
+
+import okhttp3.CompressionInterceptor
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okio.BufferedSource
+
+object Brotli : Interceptor, CompressionInterceptor.DecompressionAlgorithm {
+    override fun name(): String = "br"
+
+    override fun decompress(source: BufferedSource): BufferedSource = source
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        return BrotliInterceptor.intercept(chain)
+    }
+
+    fun OkHttpClient.Builder.brotli(): OkHttpClient.Builder {
+        return this.addInterceptor(Brotli as Interceptor)
+    }
+}
